976070169732ee6318d152224264e81e0567d37f
KLEugene <kaeugene95@gmail.com>
2024-08-14 19:25:36 +0100

    fixed modal sorting

--- js/sort-modal.js
+++ js/sort-modal.js
@@ -1,5 +1,7 @@
 const openSorting = document.getElementById("open-sorting");
 const sortingBackdrop = document.getElementById("sorting")
+const sortText = document.getElementById("sort")
+const sortList = document.getElementById("sort-list")
 console.log(sortingBackdrop)
 console.log(openSorting);
 openSorting.addEventListener("click", toggleSorting)
