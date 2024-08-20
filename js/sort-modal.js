759110ff8472bd74a623b2260327c8d42841ee21
KLEugene <kaeugene95@gmail.com>
2024-08-20 18:53:45 +0100

    fixed js code

--- js/sort-modal.js
+++ js/sort-modal.js
@@ -2,10 +2,18 @@ const openSorting = document.getElementById("open-sorting");
 const sortingBackdrop = document.getElementById("sorting")
 const sortText = document.getElementById("sort")
 const sortList = document.getElementById("sort-list")
+
 console.log(sortingBackdrop)
 console.log(openSorting);
+
 openSorting.addEventListener("click", toggleSorting)
 sortingBackdrop.addEventListener("click", toggleSorting)
+sortList.addEventListener("click", choiceEvent)
+
+function choiceEvent(event) { 
+    console.dir(event.target)
+    sortText.innerHTML = event.target.innerHTML;
+}
 function toggleSorting() { 
 sortingBackdrop.classList.toggle("is-hidden")
 }
