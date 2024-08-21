9498934f292aec3e36bdc478b42a24bb0b7cf701
Valentшna-K <kvalentyna@hotmail.com>
2024-08-21 16:34:21 +0300

    fix choiceEvent

--- js/sort-modal.js
+++ js/sort-modal.js
@@ -1,19 +1,19 @@
 const openSorting = document.getElementById("open-sorting");
-const sortingBackdrop = document.getElementById("sorting")
-const sortText = document.getElementById("sort")
-const sortList = document.getElementById("sort-list")
+const sortingBackdrop = document.getElementById("sorting");
+const sortText = document.getElementById("sort");
+const sortList = document.getElementById("sort-list");
 
-console.log(sortingBackdrop)
-console.log(openSorting);
+openSorting.addEventListener("click", toggleSorting);
+sortingBackdrop.addEventListener("click", toggleSorting);
+sortList.addEventListener("click", choiceEvent);
 
-openSorting.addEventListener("click", toggleSorting)
-sortingBackdrop.addEventListener("click", toggleSorting)
-sortList.addEventListener("click", choiceEvent)
-
-function choiceEvent(event) { 
-    console.dir(event.target)
-    sortText.innerHTML = event.target.innerHTML;
+function choiceEvent(event) {
+  const elem = event.target.localName;
+  if (elem.includes("ul")) return;
+  if (elem.includes("li"))
+    sortText.innerHTML = event.target.children[0].innerHTML;
+  else sortText.innerHTML = event.target.parentElement.children[0].innerHTML;
 }
-function toggleSorting() { 
-sortingBackdrop.classList.toggle("is-hidden")
+function toggleSorting() {
+  sortingBackdrop.classList.toggle("is-hidden");
 }
