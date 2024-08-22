d8ca5c2db4d76e7293c84eafe54208da38ec4be7
KLEugene <kaeugene95@gmail.com>
2024-08-22 13:44:23 +0100

    fixed mobile-menu

---
+++ js/mobile-menu.js
@@ -0,0 +1,10 @@
+const openMenu = document.getElementById("open-mobile-menu")
+const closeMenu = document.getElementById("mobile-menu-close");
+const mobileMenu = document.getElementById("mobile-menu")
+
+openMenu.addEventListener("click", toggleMobileMenu);
+closeMenu.addEventListener("click", toggleMobileMenu);
+
+function toggleMobileMenu() { 
+    mobileMenu.classList.toggle("is-open")
+}
